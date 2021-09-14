55c181c9345379b9065fbd523dd946dac415a73e
aliceduren <aliceduren01@byui.edu>
2021-09-14 09:18:44 -0600

    commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const lastUp=new Date(). toLocaleString();
+
+document.querySelector("#year").textContent=lastUp;
+
+const pic= "https://i.kym-cdn.com/entries/icons/facebook/000/035/141/elevated-1.jpg";
+    document.querySelector("img").setAttribute('src',pic)
