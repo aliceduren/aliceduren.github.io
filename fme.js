efef680e89ad7b8848a30239f92ad2418e80f15d
aliceduren <aliceduren01@byui.edu>
2021-09-17 23:54:19 -0600

    dbdz

--- fme.js
+++ fme.js
@@ -1,27 +1,4 @@
-// const hihowrya=1+2;
-
-// document.querySelector('h1').textContent=hihowrya;
-
-// if (hihowrya==2) {
-//     document.querySelector('p').textContent=Kboii(5,2);
-// }
-
-// function Kboii(a,b) {
-//     return a+b;
-// }
-
-// function getCel(farenhiet) {
-//     return 5/9 * (farenhiet-32);
-// }
-// document.querySelector('p').innerHTML=getCel(70)
-function createParagraph() {
-    let para = document.createElement('p');
-    para.textContent = 'You clicked the button!';
-    document.body.appendChild(para);
-  }
-  
-//   const buttons = document.querySelectorAll('button');
-  
-//   for(let i = 0; i < buttons.length ; i++) {
-//     buttons[i].addEventListener('click', createParagraph);
-//   }
+function rectangled(l,w) {
+  return l*w  
+}
+document.querySelector('h1').innerHTML=rectangled(2,3)
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const lastUp=document.lastModified;
 
 document.querySelector("#year").textContent=lastUp;
 
-const pic= "https://i.kym-cdn.com/entries/icons/facebook/000/035/141/elevated-1.jpg";
+const pic= "profile.jpg";
     document.querySelector("img").setAttribute('src',pic)
