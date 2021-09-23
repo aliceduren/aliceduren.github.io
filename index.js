4681aad9518cb7be97dc77b43668e579b1c09058
aliceduren <aliceduren01@byui.edu>
2021-09-23 16:10:49 -0600

    kgoood

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ document.querySelector("#year").textContent=lastUp;
 
 // const pic= "profilere.jpg";
 //     document.querySelector("img").setAttribute('src',pic)
+let x=new Date()
+let y=x.getFullYear()
+document.getElementById("date").textContent=y
