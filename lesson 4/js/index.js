619fc653eb20ca1fa1289801a0fb4ced39b4780a
aliceduren <aliceduren01@byui.edu>
2021-10-09 14:07:17 -0600

    final

--- lesson 4/js/index.js
+++ lesson 4/js/index.js
@@ -3,20 +3,20 @@ const mainnav= document.querySelector('.navigation')
 
 hambutton.addEventListener('click',() => {mainnav.classList.toggle('responsive')},false);
 
-window.onresize =() => {if(window.innerWidth >760) mainnav.classList.toggle('responsive')};
-
-function toggleMenu1(){
-  document.getElementById('heroimage').classList.toggle("hide");
-  }
-function toggleMenu2(){
-  document.getElementById('fiveday').classList.toggle("hide");
-  }
-function toggleMenu3(){
-  document.getElementById('article').classList.toggle("hide");
-  }
-function toggleMenu4(){
-  document.getElementById('locationmap').classList.toggle("hide");
-  }
+window.onresize =() => {if(window.innerWidth > 760) mainnav.classList.toggle('responsive')};
+// didn't need this, but here just for reference
+// function toggleMenu1(){
+//   document.getElementById('heroimage').classList.toggle("hide");
+//   }
+// function toggleMenu2(){
+//   document.getElementById('fiveday').classList.toggle("hide");
+//   }
+// function toggleMenu3(){
+//   document.getElementById('article').classList.toggle("hide");
+//   }
+// function toggleMenu4(){
+//   document.getElementById('locationmap').classList.toggle("hide");
+//   }
 const lastUp=document.lastModified;
 
 document.querySelector("#year").textContent=lastUp;
