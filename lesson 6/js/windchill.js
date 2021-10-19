20916cda4da6887db3024351dffde7894addfb6d
aliceduren <aliceduren01@byui.edu>
2021-10-19 08:53:42 -0600

    das

--- lesson 6/js/windchill.js
+++ lesson 6/js/windchill.js
@@ -1,6 +1,7 @@
+//t - temp and s= windchill speed
 function windchill (t,s) {
     let f=35.74+0.6215 * t - 35.75 *s**0.16 + 0.4275*t*s**0.16
     return f
 }
 
-document.getElementById('wind').textContent=5;
+document.getElementById('wind').textContent=Math.round(windchill(60,12));
