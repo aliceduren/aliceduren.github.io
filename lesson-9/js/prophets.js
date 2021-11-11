2d0d59a8a5f8cb696a9b16d80872465a83f488d6
aliceduren <aliceduren01@byui.edu>
2021-11-10 20:37:52 -0700

    plzplzpplz

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -12,26 +12,16 @@ fetch(requestURL)
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      card.appendChild(h2);
-      document.querySelector('div.cards').appendChild(card);
-
-      let card2 = document.createElement('p');
       let h3 = document.createElement('h3');
       h3.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-      card2.appendChild(h3);
-      document.querySelector('div.cards').appendChild(card2);
-
-      let card3 = document.createElement('p');
       let a_h3 = document.createElement('h3');
       a_h3.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-      card3.appendChild(a_h3);
-      document.querySelector('div.cards').appendChild(card3);
-
-      let im = document.createElement('section');
       let image = document.createElement('img');
       image.setAttribute('src', prophets[i].imageurl);
-      im.appendChild(image);
-      document.querySelector('div.cards').appendChild(im);
-      //   document.querySelector('div.cards').appendChild(url);
-    } // temporary checking for valid response and data parsing
+      card.appendChild(h2);
+      card.appendChild(h3);
+      card.appendChild(a_h3);
+      card.appendChild(image);
+      document.querySelector('div.cards').appendChild(card);
+    }
   });
