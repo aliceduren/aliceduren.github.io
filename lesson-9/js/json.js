dcdebec04976bb0c9d15e3c11c2eed69974db6d4
aliceduren <aliceduren01@byui.edu>
2021-11-10 22:13:48 -0700

    zcc

--- lesson-9/js/json.js
+++ lesson-9/js/json.js
@@ -15,16 +15,63 @@ myFetch()
     }
     console.log(getKeyByValue(jsonData, 'Preston'));
 
-    // console.log(jsonData.towns[0].motto);
+    let fishName = document.createElement('h2');
+    fishName.innerHTML = jsonData.towns[2].name;
+    document.getElementById('townNamef').appendChild(fishName);
 
-    let h1 = document.createElement('h1');
-    h1.innerHTML = jsonData.towns[2].name;
+    let presName = document.createElement('h2');
+    presName.innerHTML = jsonData.towns[6].name;
+    document.getElementById('townNamep').appendChild(presName);
 
-    let h3 = document.createElement('h3');
-    h3.innerHTML = jsonData.towns[3].motto;
+    let sodaName = document.createElement('h2');
+    sodaName.innerHTML = jsonData.towns[0].name;
+    document.getElementById('townNames').appendChild(sodaName);
 
-    document.getElementById('FishHaven').appendChild(h1);
-    document.querySelector('#readyForClass').innerHTML =
-      jsonData.towns[3].motto;
+    let fishMotto = document.createElement('h3');
+    fishMotto.innerHTML = jsonData.towns[2].motto;
+    document.getElementById('townMottof').appendChild(fishMotto);
+
+    let presMotto = document.createElement('h3');
+    presMotto.innerHTML = jsonData.towns[6].motto;
+    document.getElementById('townMottop').appendChild(presMotto);
+
+    let sodaMotto = document.createElement('h3');
+    sodaMotto.innerHTML = jsonData.towns[0].motto;
+    document.getElementById('townMottos').appendChild(sodaMotto);
+
+    // let image = document.createElement('img');
+    // image.setAttribute('src', towns[2].photo);
+    // document.getElementById('townMottos').appendChild(sodaMotto);
   })
   .catch((e) => console.log(e));
+
+// let card = document.createElement('section');
+// let h2 = document.createElement('h2');
+// h2.textContent = towns[1].name;
+// document.querySelector('.try').appendChild(card);
+// const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+// fetch(requestURL)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (jsonObject) {
+//     console.table(jsonObject);
+//     const towns = jsonObject['towns'];
+//     for (let i = 0; i < towns.length; i++) {
+//       let card = document.createElement('section');
+//       let h2 = document.createElement('h2');
+//       h2.textContent = towns[i].name;
+//       let h3 = document.createElement('h3');
+//       h3.textContent = 'Date of Birth: ' + towns[i].motto;
+//       let a_h3 = document.createElement('h3');
+//       a_h3.textContent = 'Place of Birth: ' + towns[i].yearFounded;
+//       let image = document.createElement('img');
+//       image.setAttribute('src', towns[i].photo);
+//       card.appendChild(h2);
+//       card.appendChild(h3);
+//       card.appendChild(a_h3);
+//       card.appendChild(image);
+//       document.querySelector('.content').appendChild(card);
+//     }
+//   });
