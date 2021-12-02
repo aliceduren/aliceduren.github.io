34f029900c6bd648abc5ed2d3ccbf1417acabaa2
aliceduren <aliceduren01@byui.edu>
2021-12-01 17:11:14 -0700

    ads

--- lesson-12/siteplan/index.js
+++
@@ -1,7 +0,0 @@
-let x= new Date();
-let y=x.getFullYear();
-document.getElementById('date').textContent=y;
-
-const lastUp=document.lastModified;
-
-document.querySelector("#year").textContent=lastUp;
