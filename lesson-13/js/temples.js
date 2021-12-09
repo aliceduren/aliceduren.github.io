aaa593433532b9d2a9dfbf1e0f60ae566a4e9a63
aliceduren <aliceduren01@byui.edu>
2021-12-09 13:37:56 -0700

    tea

--- lesson-13/js/temples.js
+++ lesson-13/js/temples.js
@@ -1,32 +1,97 @@
-fetch('https://nathansbradshaw.github.io/temples2.json')
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    for (let i = 0; i < jsObject.length; i++) {
+// fetch('https://nathansbradshaw.github.io/temples2.json')
+//   .then((response) => response.json())
+//   .then((jsObject) => {
+//     console.log(jsObject);
+//     for (let i = 0; i < jsObject.length; i++) {
+//       let card = document.createElement('section');
+//       let image = document.createElement('img');
+//       image.setAttribute('src', jsObject[i].imageurl);
+//       image.alt = jsObject[i].name;
+//       let h2 = document.createElement('h2');
+//       h2.textContent = jsObject[i].name;
+//       let h3 = document.createElement('h3');
+//       h3.textContent = 'Phone: ' + jsObject[i].phone;
+//       let p = document.createElement('h3');
+//       p.textContent = 'Address: ' + jsObject[i].address1;
+//       let p2 = document.createElement('h3');
+//       p2.textContent = 'Services: ';
+//       let p22 = document.createElement('p');
+//       p22.textContent = jsObject[i].services[0];
+//       let p3 = document.createElement('p');
+//       p3.textContent = jsObject[i].services[1];
+//       let p4 = document.createElement('p');
+//       p4.textContent = jsObject[i].services[2];
+//       let p5 = document.createElement('p');
+//       p5.textContent = jsObject[i].services[3];
+//       let p55 = document.createElement('h3');
+//       p55.textContent = 'Fun Fact: ';
+//       let p6 = document.createElement('p');
+//       p6.textContent = jsObject[i].Summary.facts[0];
+
+//       card.appendChild(image);
+//       card.appendChild(h2);
+//       card.appendChild(h3);
+//       card.appendChild(p);
+//       card.appendChild(p2);
+//       card.appendChild(p22);
+//       card.appendChild(p3);
+//       card.appendChild(p4);
+//       card.appendChild(p5);
+//       card.appendChild(p55);
+//       card.appendChild(p6);
+
+//       document.querySelector('div.cards').appendChild(card);
+//     }
+//   });
+// fetch('https://nathan-byui-api.herokuapp.com/temples/', {
+//   headers: {
+//     apiKey:
+//       'Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68XwZj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((jsObject) => {
+//     console.log(jsObject);
+//   });
+// for (let i = 0; i < jsObject.length; i++) {
+temples = [109, 108, 103, 198];
+temples.forEach((templeid) => {
+  fetch(`https://nathan-byui-api.herokuapp.com/temples/${templeid}`, {
+    headers: {
+      apiKey:
+        'Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68XwZj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N',
+    },
+  })
+    .then((response) => response.json())
+    .then((jsObject) => {
+      console.log(jsObject);
+
       let card = document.createElement('section');
       let image = document.createElement('img');
-      image.setAttribute('src', jsObject[i].imageurl);
-      image.alt = jsObject[i].name;
+      image.setAttribute('src', jsObject.imageurl);
+      image.alt = jsObject.city;
       let h2 = document.createElement('h2');
-      h2.textContent = jsObject[i].name;
+      h2.textContent = jsObject.name;
       let h3 = document.createElement('h3');
-      h3.textContent = 'Phone: ' + jsObject[i].phone;
+      h3.textContent = 'Phone: ' + jsObject.phone;
       let p = document.createElement('h3');
-      p.textContent = 'Address: ' + jsObject[i].address1;
+      p.textContent = 'Address: ' + jsObject.address1;
       let p2 = document.createElement('h3');
       p2.textContent = 'Services: ';
       let p22 = document.createElement('p');
-      p22.textContent = jsObject[i].services[0];
+      p22.textContent = jsObject.services[0];
       let p3 = document.createElement('p');
-      p3.textContent = jsObject[i].services[1];
+      p3.textContent = jsObject.services[1];
       let p4 = document.createElement('p');
-      p4.textContent = jsObject[i].services[2];
+      p4.textContent = jsObject.services[2];
       let p5 = document.createElement('p');
-      p5.textContent = jsObject[i].services[3];
+      p5.textContent = jsObject.services[3];
+      let ptext = document.createElement('h3');
+      ptext.textContent = 'Closure Schedule';
       let p55 = document.createElement('h3');
-      p55.textContent = 'Fun Fact: ';
+      p55.textContent = 'History: ';
       let p6 = document.createElement('p');
-      p6.textContent = jsObject[i].Summary.facts[0];
+      p6.textContent = jsObject.Summary.facts;
 
       card.appendChild(image);
       card.appendChild(h2);
@@ -37,12 +102,41 @@ fetch('https://nathansbradshaw.github.io/temples2.json')
       card.appendChild(p3);
       card.appendChild(p4);
       card.appendChild(p5);
+      card.appendChild(ptext);
+      for (const key of Object.keys(jsObject.closures)) {
+        // console.log(jsObject.closures[key]);
+
+        jsObject.closures[key].forEach((closure) => {
+          console.log(closure);
+          let p7 = document.createElement('p');
+          p7.textContent =
+            'Startdate: ' +
+            closure.startdate +
+            ' - ' +
+            'Enddate: ' +
+            closure.enddate;
+
+          card.appendChild(p7);
+        });
+      }
       card.appendChild(p55);
       card.appendChild(p6);
 
       document.querySelector('div.cards').appendChild(card);
-    }
-  });
+    });
+  //   if (
+  //     jsObject[i].temple_id == 101 ||
+  //     jsObject[i].temple_id == 79 ||
+  //     jsObject[i].temple_id == 198 ||
+  //     jsObject[i].temple_id == 205
+  //   )
+  //   let card = document.createElement('section');
+  //   card.setAttribute('class', jsObject[i].temple_id);
+  //   let tname = document.createElement('h2');
+  //   tname.textContent = jsObject[i].name;
+  //   card.appendChild(tname);
+  //   document.querySelector('.div.cards').appendChild(card);
+});
 
 function windchill(t, s) {
   let f = 35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16;
